1029e9291a5588c7a296d17be31477289aefea47
Tom Theisen <github@tomtheisen.com>
2020-04-30 19:54:28 -0700

    Add project files.

---
+++ CsvParser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class CsvParser {
+    public static List<List<string>> ReadCsvFromString(string contents) {
+        using var ms = new MemoryStream(Encoding.UTF8.GetBytes(contents));
+        using var reader = new StreamReader(ms);
+        return ReadCsv(reader);
+    }
+
+    public static List<List<string>> ReadCsv(string fileName) {
+        using var reader = new StreamReader(File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+        return ReadCsv(reader);
+    }
+
+    public static List<List<string>> ReadCsv(TextReader reader) {
+        var result = new List<List<string>>();
+        var currentRow = new List<string>();
+        var currentCell = new StringBuilder();
+        int c;
+
+        void AddCell() {
+            currentRow.Add(currentCell.ToString());
+            currentCell.Clear();
+        }
+
+        void AddRow() {
+            AddCell();
+            result.Add(currentRow);
+            currentRow = new List<string>();
+        }
+
+        void ReadNewLine() {
+            if (reader.Read() != '\n') throw new FormatException("Found carriage return without following linefeed");
+            AddRow();
+        }
+
+        Start: switch (c = reader.Read()) {
+            case '\r':
+                ReadNewLine();
+                goto Start;
+            case '"':
+                goto QuotedCell;
+            case int _ when c < 0:
+                if (currentRow.Count > 0) result.Add(currentRow);
+                return result;
+            default:
+                currentCell.Append((char)c);
+                goto RawCell;
+        }
+
+        QuotedCell: switch (c = reader.Read()) {
+            case '"':
+                goto EndQuote;
+            case int _ when c < 0:
+                throw new FormatException($"Unterminated quoted value encountered in record {result.Count + 1}");
+            default:
+                currentCell.Append((char)c);
+                goto QuotedCell;
+        }
+
+        EndQuote: switch (c = reader.Read()) {
+            case '\r':
+                ReadNewLine();
+                goto Start;
+            case ',':
+                AddCell();
+                goto Start;
+            case '"':
+                currentCell.Append('"');
+                goto QuotedCell;
+            case int _ when c < 0:
+                AddCell();
+                return result;
+            default:
+                throw new FormatException($"Unexpected character following trailing quote '{(char)c}'");
+        }
+
+        RawCell: switch (c = reader.Read()) {
+            case '"':
+                throw new FormatException("Encountered quote character (\") in unquoted value");
+            case ',':
+                AddCell();
+                goto Start;
+            case '\r':
+                ReadNewLine();
+                goto Start;
+            case int _ when c < 0:
+                AddRow();
+                return result;
+            default:
+                currentCell.Append((char)c);
+                goto RawCell;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using static CsvParser;
+
+class Program {
+    static void Main() {
+        TestSuccess();
+        TestFailure();
+    }
+
+    private static void TestFailure() {
+        string[] failureCases = {
+            "a\rb",             // bad line terminator
+            "a\"b,c\"d",        // quote after non-quoted data character
+            "\"a",              // unterminated quote
+            "\"a\"b",           // unexpected data character after close quote
+        };
+
+        bool inappropriateSuccess = false;
+        foreach (var item in failureCases) {
+            try {
+                ReadCsvFromString(item);
+                inappropriateSuccess = true;
+                Console.Error.WriteLine("Testing Case: \n" + item + "\nUnexpected parsing success.");
+            }
+            catch { /* working as intended */ }
+        }
+        if (!inappropriateSuccess) Console.WriteLine("Task failed successfully.\n");
+    }
+
+    private static void TestSuccess() {
+        const string document =
+            "a,b\r\n" +
+            "\"d\"\"d\",\"e,e\",\"f\r\nf\"\r\n";
+
+        var expected = new[] {
+            new[] { "a", "b" },
+            new[] { "d\"d", "e,e", "f\r\nf" },
+        };
+
+        var actual = ReadCsvFromString(document);
+
+        // not really a robust test, because it doesn't test for *extra* cells or rows
+        // but good enough
+        bool foundError = false;
+        for (int i = 0; i < expected.Length; i++) {
+            for (int j = 0; j < expected[i].Length; j++) {
+                if (expected[i][j] != actual[i][j]) {
+                    foundError = true;
+                    Console.Error.WriteLine("Problem at {0}, {1}.  Expected:\n{2} Actual:\n{3}", i, j, expected[i][j], actual[i][j]);
+                }
+            }
+        }
+        if (!foundError) Console.WriteLine("CSV parsed successfully");
+    }
+}
